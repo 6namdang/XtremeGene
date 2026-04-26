bf48d3e4b226e23e80a8d155f5e896f62b79ae6e
Hoang Nam Dang <hoangdang@Hoangs-MacBook-Pro-3.local>
2026-04-26 03:13:27 -0400

    commit

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/hoangdang/Downloads/XtremeGene/backend/venv
+set -gx VIRTUAL_ENV /Users/hoangdang/Documents/XtremeGene/backend/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
